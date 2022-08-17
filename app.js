5e59326764491288203746ae6d76cef4a66c6159
gonellapol <gonellapol@hotmail.com>
2022-08-16 23:24:50 -0300

    agrego path carrito y sign in

--- app.js
+++ app.js
@@ -13,3 +13,12 @@ app.listen(3000, () =>{
 app.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname, '/views/home.html'));
 })
+
+app.get('/signin', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/views/signin.html'));
+})
+
+app.get('/carrito', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/views/carrito.html'));
+})
+
