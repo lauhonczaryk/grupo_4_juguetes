ed743dde27ea85af42288ff08a3488d254523fda
lauhonczaryk <lchonczaryk@gmail.com>
2022-08-24 20:38:35 -0300

    html de home y css primera version

--- app.js
+++ app.js
@@ -14,11 +14,16 @@ app.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname, '/views/home.html'));
 })
 
-app.get('/signin', (req,res) =>{
-    res.sendFile(path.join(__dirname, '/views/signin.html'));
+app.get('/login', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/views/login.html'));
 })
 
 app.get('/carrito', (req,res) =>{
     res.sendFile(path.join(__dirname, '/views/carrito.html'));
 })
+
+app.get('/register', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+})
+
 
