c998814f740dccaf644de78b6ee9c120595627af
Patricio Maldonado <patriciomaldonado@patriciacstudio.lan>
2022-09-14 10:27:59 -0300

    agregando partials

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 
+var indexRouter = require('./routes/index');
+
 const app = express();
 
 const path = require('path');
 
-app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "./views"));
+app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use('/', indexRouter);
 
 app.listen(3000, () =>{
     console.log('Servidor corriendo');
