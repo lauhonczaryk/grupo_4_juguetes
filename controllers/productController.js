5642c583f6c76f42792265898edf5198ff6f9314
gonellapol <gonellapol@hotmail.com>
2022-09-22 12:37:42 -0300

    Sprint 3

--- controllers/productController.js
+++ controllers/productController.js
@@ -10,6 +10,12 @@ const data = JSON.parse(jsonData);
 return data;
 }
 
+//Fucncion para pasar data a JSON y mandarla al .JSON
+
+function writeFile(data){
+    const dataString = JSON.stringify(data, null, 5);
+    fs.writeFileSync(path.join(__dirname,"../data/productDataBase.json"), dataString);
+}
 
 const productController = {
     crear: function (req, res) {
@@ -25,6 +31,57 @@ const productController = {
            return producto.id == req.params.id
         });
         res.render('product-detail', { products: productoEncontrado });
+    },
+    store: function (req, res){
+        const data = findAll()
+        
+        const newProduct = {
+            id: data.length + 1, //Cuenta cuantos elementos hay en la lista y le suma 1
+            name: req.body.name,
+            price: req.body.price, 
+            category: req.body.category,
+            description: req.body.description,
+        }
+         data.push(newProduct); //Le agrego el nuevo product al data
+        writeFile(data);
+
+        res.redirect("/productos/listar");
+
+        
+    },
+    edit: function (req, res){
+        const data = findAll();
+        const productoEncontrado = data.find(function(producto) {
+           return producto.id == req.params.id
+        });
+
+        res.render("edit",{ producto: productoEncontrado});
+    },
+    update: function (req, res){
+        const data = findAll();
+        const productoEncontrado = data.find(function(producto) {
+           return producto.id == req.params.id
+        });
+        
+        productoEncontrado.name = req.body.name;
+        productoEncontrado. price = req.body.price;
+        productoEncontrado. description = req.body.description;
+        productoEncontrado. category = req.body.category;
+        writeFile(data);
+       
+
+        res.redirect("/productos/listar");
+    },
+    destroy: function (req, res){
+        const data = findAll();
+        const productoEncontrado = data.findIndex(function(producto) { //El findindex de devueve en que indice del elemento del array donde esta 
+           return producto.id == req.params.id
+        });
+        data.splice(productoEncontrado,1);
+        writeFile(data);
+
+        res.redirect("/productos/listar");
+
     }
 }
 
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -7,5 +7,8 @@ const productController = require("../controllers/productController");
 router.get('/listar', productController.list);
 router.get('/crear', productController.crear);
 router.get('/detalle/:id', productController.detail);
-
+router.post('/crear', productController.store);
+router.get('/editar/:id', productController.edit);
+router.put('/editar/:id', productController.update);
+router.delete('/borrar/:id', productController.destroy);
 module.exports = router;
