336ad557b9bd716334642046012f0305f0a46a98
Patricio Maldonado <63500870+patriciomaldonado@users.noreply.github.com>
2022-10-05 12:41:48 -0300

    middleware validations

---
+++ middlewares/validateRegisterMiddleware.js
@@ -0,0 +1,14 @@
+const { body } = require('express-validator');
+
+const validations = [
+    body('nombreApellido').notEmpty().withMessage('Te faltó poner tu nombre'),
+    body('usuario').notEmpty().withMessage('Te faltó poner tu usuario'),
+    body('email')
+        .notEmpty().withMessage('Te faltó poner tu correo electrónico').bail()
+        .isEmail().withMessage('Tenes que escribir un correo válido'),
+    body('password')
+        .notEmpty().withMessage('Te faltó poner tu contranseña').bail()
+        .isLength({min: 4, max:22}).withMessage('Tu contraseña debe contener un minimo de 4 cáracteres')
+];
+
+module.exports = validations;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,24 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { body } = require('express-validator');
 
 // CONTROLLER
-
 const userController = require("../controllers/userController");
 
 
-// MIDDLEWARE
+// MIDDLEWARES
+const validations = require('../middlewares/validateRegisterMiddleware');
 
-const validations = [
-    body('nombreApellido').notEmpty().withMessage('Te faltó poner tu nombre'),
-    body('usuario').notEmpty().withMessage('Te faltó poner tu usuario'),
-    body('email')
-        .notEmpty().withMessage('Te faltó poner tu correo electrónico').bail()
-        .isEmail().withMessage('Tenes que escribir un correo válido'),
-    body('password')
-        .notEmpty().withMessage('Te faltó poner tu contranseña').bail()
-        .isLength({min: 4, max:22}).withMessage('Tu contraseña debe contener un minimo de 4 cáracteres')
-];
 
 // Formulario de registro
 router.get('/register', userController.register);
