914e6fad8b6f9658aa874574dd989d14b6c32c5b
Patricio Maldonado <63500870+patriciomaldonado@users.noreply.github.com>
2022-11-08 19:23:04 -0300

    arreglando css index

--- middlewares/guestMiddleware.js
+++ middlewares/guestMiddleware.js
@@ -1,6 +1,6 @@
 function guestMiddleware (req, res, next){
     if(req.session.userLogged){
-        res.redirect('/user/profile');
+        return res.redirect('/user/profile');
     }
     next()
 }
