ee982072719547b89f1b56a7057f77343fa1b3a1
lauhonczaryk <lchonczaryk@gmail.com>
2023-01-09 17:30:43 -0300

    iconos acomodados

--- middlewares/validateCreationMiddleware.js
+++ middlewares/validateCreationMiddleware.js
@@ -4,7 +4,12 @@ const validateCreationMiddleware = [
     body('name').notEmpty().withMessage('Ingresá el nombre del producto').bail().isLength({min:5}).withMessage('Mínimo 5 caractéres'),
     body('description').notEmpty().withMessage('Ingresá una descrpción para este producto').bail().isLength({min:20}).withMessage('Mínimo 20 caractéres'),
     body('productImage')
-        .notEmpty().withMessage('Ingresá la foto del producto'),
+        .notEmpty().withMessage('Ingresá la foto del producto').custom(
+            (value, { req }) => {
+                const acceptedFileExtensions = [".jpg", ".png", ".jpeg"];
+                return acceptedFileExtensions.includes(path.extname(req.file.originalname));
+            }
+        ).withMessage("Ingresá una foto con estos formatos: .jpg, .png y .jpeg"),
     body('category_id').notEmpty().withMessage('Ingresá el id de la categoría correspondiente: 1(Videojuegos) - 2(Aire libre) - 3(Juegos de mesa) - 4(Juguetes)'),
     body('price').notEmpty().withMessage('Ingresá un precio (sin comas, puntos ni símbolos)')
 ];
